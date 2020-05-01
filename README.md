b5640757429a749660dd44dc732d9b72d545c464
Thomas Fritzsche <tf@noto.de>
2020-05-01 23:00:54 +0200

    provide array buffer

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # morse_player
-
--- morse.js
+++ morse.js
@@ -1,1 +1,42 @@
-console.log("Hallo Welt!");
+
+
+const button = document.querySelector('button');
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+button.onclick = function() {
+    play(audioCtx);
+}    
+
+function play(ctx) {
+    let freq = 700;
+    let rt = 50;
+    let ft = 50;
+    let myArrayBuffer = ctx.createBuffer(2, ctx.sampleRate * 1, ctx.sampleRate);
+
+    // Fill the buffer with white noise;
+    //just random values between -1.0 and 1.0
+    for (let channel = 0; channel < myArrayBuffer.numberOfChannels; channel++) {
+        // This gives us the actual ArrayBuffer that contains the data
+        let nowBuffering = myArrayBuffer.getChannelData(channel);
+        for (let i = 0; i < myArrayBuffer.length; i++) {
+            nowBuffering[i] = Math.sin(2 * Math.PI * freq * i / ctx.sampleRate);
+            if (i < rt) {
+                nowBuffering[i] *= Math.pow(Math.sin(Math.PI * i / (2 * rt)), 2);
+            }
+            if (i > myArrayBuffer.length - ft) {
+                nowBuffering[i] *= Math.pow((Math.sin(2 * Math.PI * (i - (myArrayBuffer.length - ft) + ft) / (4 * ft))), 2);
+            }
+        }
+    }
+    // Get an AudioBufferSourceNode.
+    // This is the AudioNode to use when we want to play an AudioBuffer
+    let source = ctx.createBufferSource();
+    // set the buffer in the AudioBufferSourceNode
+    source.buffer = myArrayBuffer;
+    // connect the AudioBufferSourceNode to the
+    // destination so we can hear the sound
+    source.connect(ctx.destination);
+    // start the source playing
+    source.start();
+}
